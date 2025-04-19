a44107322d3f0e4141ca002623b12cc8f06c6223
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2025-04-20 00:35:40 +0530

    updated algorithm and server

--- project_backend/routes/eightqueenspuzzle.js
+++ project_backend/routes/eightqueenspuzzle.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import { supabase } from '../server.js';
 
 // sample
-router.post('/eightqueenpuzzle', async (req, res) => {
+router.post('/', async (req, res) => {
     const { data, error } = await supabase
         .from('eight_queen_puzzle')
         .insert([{ player_solution: { player_name: 'Bilal', solution: [7, 6, 5, 4, 3, 2, 1, 0] } }]);
@@ -14,14 +14,23 @@ router.post('/eightqueenpuzzle', async (req, res) => {
 });
 
 // Save solutions, including sequential and threaded algorithms'
-router.post('/eightqueenpuzzle/solutions', async (req, res) => {
-    // const { count, solution, time_taken } = req.body;
+router.post('/solutions', async (req, res) => {
+    const { thrTimeTaken, thrCount, thrSolutions, seqTimeTaken, seqCount, seqSolutions } = req.body;
+
+    console.log("sequential: " + thrTimeTaken);
+    console.log("threaded: " + thrSolutions);
+
+
+    // Directly use the destructured values in the insert query
     const { data, error } = await supabase
         .from('eight_queen_puzzle')
         .insert([{
-            sequential_solution_count: 10,
-            sequential_solution: [1,2,3],
-            sequential_time_taken: 1.5
+            sequential_solution_count: seqCount,
+            sequential_solution: seqSolutions,
+            sequential_time_taken: seqTimeTaken,
+            threaded_solution_count: thrCount,
+            threaded_solution: thrSolutions,
+            threaded_time_taken: thrTimeTaken
         }]);
 
     if (error) return res.status(400).json({ error });
