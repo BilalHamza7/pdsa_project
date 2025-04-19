2e9ba58babd2cb9279af1b63b24d1badfefcdbbf
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2025-04-19 15:08:46 +0530

    updated frontend route bug

--- project_frontend/src/main.jsx
+++ project_frontend/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './main.css';
 import Home from './games/home';
 import EightQueensGame from './games/eightqueen_game/components/main';
-import knightstour_main from './games/knighttour_game/main';
+import knightstour_main from './games/knighttour_game/knightstour_main';
 import TicTacToeGame from './games/tictactoe_game/main';
 import TowerOfHanoiGame from './games/towerofhanoi_game/main';
 import TravelingSalesmanGame from './games/travelingsalesman_game/main';
