41f3d4c703cac6a673b215ec248877d4f6319fc8
vihanga10 <vihangamadushamini@gmail.com>
2025-04-27 15:59:19 +0530

    update

--- project_frontend/src/games/towerofhanoi_game/jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest', // handles .js/.jsx files
-    },
-    moduleFileExtensions: ['js', 'jsx'],
-  };
-  
